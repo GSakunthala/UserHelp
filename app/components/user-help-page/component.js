4b61f00b9e81109aeb5507439eaed97286a2f20b
Gnanasekar <532968@cognizant.com>
2017-08-10 12:39:21 +0530

    localstorage worked

--- app/components/user-help-page/component.js
+++ app/components/user-help-page/component.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import moment from 'moment';
 import {
   UserValidations
 } from './validations';
@@ -44,43 +45,19 @@ export default Ember.Component.extend(UserValidations,{
 	{
 		onCallMeBack()
 		{
-
-			/*let userDetails = 
-			{
-				'firstName' : this.get('firstName'),	
-				'lastName' : this.get('lastName')	,
-				'email' : this.get('email'),
-				'phone' : this.get('phone')	
-			} */
 			let userDetails = {};
 			let fields =this.get('fields');
-			//var userObject = Ember.Object.create();
-			var thiscomponent = this;
-			fields.forEach(function(field,i){
-				console.log(i+"--"+field.id);
+			let thiscomponent = this;
+			fields.forEach(function(field){
 				let fieldValue = thiscomponent.get(field.id);
-				//userObject.set(field.id,fieldValue);
 				userDetails[field.id] = fieldValue;
-
-
 			});
-						console.log("userDetaisl--"+userDetails);
-
-			this.setProperties(
-    		{
-    			'userDetails':userDetails,
-    			'isValidForm':true
-    		}
-    		);
+			this.setProperties({'userDetails':userDetails,'isValidForm':true});
 			this.showStatusMessage();	
 			var store = this.get('store');
-
 			var storeRecord = store.createRecord('user', userDetails);
-			storeRecord.save().then((response) => {console.log("response--"+response)}).catch((reason) => {console.log("Reason--"+reason)});
-				}
+			storeRecord.save();
+		}
 	}
-
-
-
 
 });
--- app/components/user-help-page/user-input/component.js
+++ app/components/user-help-page/user-input/component.js
@@ -7,22 +7,19 @@ isError:Ember.computed.and('isInvalid','isNotFocussed','hasContent'),
 isErrorfree : Ember.computed.and('isValid','isNotFocussed','hasContent'),
   init()
   {		
-
-  		this._super(...arguments);
-  		let fieldName = this.get('fieldName');
+this._super(...arguments);
+let fieldName = this.get('fieldName');
 this.set('value',Ember.computed.alias(`model.${fieldName}`));
-  this.setProperties({'isInvalid': Ember.computed.readOnly(`model.validations.attrs.${fieldName}.isInvalid`)});
-  this.setProperties({'isValid': Ember.computed.readOnly(`model.validations.attrs.${fieldName}.isTruelyValid`)});
-
-
-  },
+this.setProperties({'isInvalid': Ember.computed.readOnly(`model.validations.attrs.${fieldName}.isInvalid`)});
+this.setProperties({'isValid': Ember.computed.readOnly(`model.validations.attrs.${fieldName}.isTruelyValid`)});
+ },
     focusIn()
   {
-  	this.set('isNotFocussed',false);
+this.set('isNotFocussed',false);
   },
   focusOut()
   {
-  	  	this.set('isNotFocussed',true);
+this.set('isNotFocussed',true);
 
   }
 
